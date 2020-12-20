c5718c91a90b8caaa3db4db68e0d9639bbea303c
lishanxin <34839411+lishanxin@users.noreply.github.com>
2020-12-20 20:27:47 +0800

    JSX布局示例

--- App.js
+++ App.js
@@ -7,43 +7,46 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
-  }
+import {Image, StyleSheet, Text, View, TextInput, ScrollView, Button} from 'react-native';
+const data = 'https://p3.ssl.qhimgs1.com/sdr/_240_/t017a90c496eb0c4593.jpg';
+export default class App extends Component{
+	render() {
+		return (
+			<ScrollView >
+				<View style={styles.container}>
+					<Text style={styles.welcome}>Hello, world!</Text>
+					<Image style={{height:100, width:100}} source={{uri:data}} />
+					<TextInput style={{
+						width:'100%', backgroundColor:'grey'}} />
+				</View>
+				<View style={styles.container} >
+					<Text style={styles.welcome}>Text2</Text>
+					<Image style={{height:100, width:100}} source={{uri:data}} />
+				</View>
+				<View style={styles.container}  >
+					<Text style={styles.welcome}>Text3</Text>
+					<Image style={{height:100, width:100}} source={{uri:data}} />
+				</View>
+			</ScrollView>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+	container: {
+		// flex: 1,
+		// justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: '#F5FCFF',
+	},
+	welcome: {
+		fontSize: 20,
+		textAlign: 'center',
+		margin: 10,
+	},
+	instructions: {
+		textAlign: 'center',
+		color: '#333333',
+		marginBottom: 5,
+	},
 });
