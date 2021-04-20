a8fb88fd957a4541a56727c36739af1c438aeb1d
lishanxin <34839411+lishanxin@users.noreply.github.com>
2021-04-20 15:49:05 +0800

    Update index.js

--- index.js
+++ index.js
@@ -7,3 +7,14 @@ import App from './App';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
+
+/** __attribute__ 报错
+ 
+static BOOL RCTParseUnused(const char **input)
+{
+  return RCTReadString(input, "__unused") || 
+          RCTReadString(input, "__attribute__((__unused__))") ||
+         RCTReadString(input, "__attribute__((unused))");
+}
+
+ */
