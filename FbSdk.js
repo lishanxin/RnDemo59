bf431f22e67c3e90929a56eb983633e3d1883e2d
lishanxin <34839411+lishanxin@users.noreply.github.com>
2021-04-20 15:28:02 +0800

    可进行登录
    IOS需要FB应用才可登录；Android不需要FB应用

--- FbSdk.js
+++ FbSdk.js
@@ -1,12 +1,39 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { View,Text } from 'react-native';
 import { LoginButton, AccessToken } from 'react-native-fbsdk';
 
+import { LoginManager } from "react-native-fbsdk";
+
+// ...
+
+
 export default class Login extends Component {
+    loginClick = () => {
+
+        // Attempt a login using the Facebook login dialog asking for default permissions.
+        LoginManager.logInWithPermissions(["public_profile"]).then(
+            function(result) {
+            if (result.isCancelled) {
+                console.log("Login cancelled");
+            } else {
+                console.log(
+                "Login success with permissions: " +
+                    result.grantedPermissions.toString()
+                );
+            }
+            },
+            function(error) {
+            console.log("Login fail with error: " + error);
+            }
+        );
+    }
+
   render() {
           return (
       <View style={{marginTop:200}}>
+          <Text onPress={this.loginClick}>Login</Text>
 				<LoginButton
+          permissions={['email,public_profile']}
           onLoginFinished={
             (error, result) => {
               if (error) {
@@ -14,10 +41,15 @@ export default class Login extends Component {
               } else if (result.isCancelled) {
                 console.log("login is cancelled.");
               } else {
+                console.log("login is succeed.");
                 AccessToken.getCurrentAccessToken().then(
                   (data) => {
                     console.log(data.accessToken.toString())
+
+                    // 获取用户信息
+                    // curl -i -X GET  "https://graph.facebook.com/126407302859177?fields=picture,name,id,email,gender,permissions&access_token=EAAE2y46DTRQBAEdTZCVVcIYKAs1mpXc2QfuuUe2FqrkWKmsQstelAZBZBsdV3NVM5JSdVuhtSQQQwRXzHtIE3dpXK4ZBc6KdBAKcqSTMZADRT68uHLv8CvxjLii7Ktj0EyCkvFxFMeiy0LINBIfqbtIbwYP6MmfEG9h3lnxOa4vVZBtPn1zay2VPtZC9aUlj2VkPViVapAZCer24JWZBsPeuU"
                   }
+                
                 )
               }
             }
